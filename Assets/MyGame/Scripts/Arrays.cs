dbe3064f06e3c249e9e736e1bee2223f57894ff4
GlendaLP <114597911+GlendaLP@users.noreply.github.com>
2022-11-22 21:52:44 +0100

    Add files via upload

---
+++ Assets/MyGame/Scripts/Arrays.cs
@@ -0,0 +1,136 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class Arrays : MonoBehaviour
+{
+
+    int[] array;
+
+    int[] array1 = new int[4];
+    int[] array2 = new int[4] { 21, 22, 23, 24 };
+    int[] array3 = new int[7] { 31, 32, 33, 34, 35, 36, 37 };
+    int[] array5 = new int[48];
+    int[] array6 = new int[498];
+    int[] array7 = new int[498];
+    string[] array8 = new string[100];
+    int[] array9 = new int[1000];
+    int[] array11 = new int[1000];
+    int[] array12 = new int[1000];
+
+    string langenString;
+    string[] array4 = new string[10];
+
+
+
+    public void Start()
+    {
+        GameObject[] arrayColoredImagesRed = GameObject.FindGameObjectsWithTag("RedImage");
+
+        Debug.Log(array1[0]);
+        Debug.Log(array2[0]);
+        Debug.Log(array3[0]);
+        Debug.Log(array4[0]);
+
+        Debug.Log(array1[array1.Length-1]);
+        Debug.Log(array2[array2.Length-1]);
+        Debug.Log(array3[array3.Length-1]);
+        Debug.Log(array4[array4.Length-1]);
+
+        array1[3] = 3;
+        Debug.Log(array1[3]);
+
+        array2[2] = 203;
+        Debug.Log(array2[2]);
+
+        array3[5] = 333;
+        Debug.Log(array3[5]);
+
+        array4[2] = "heute";
+        Debug.Log(array4[2]);
+
+        arrayColoredImagesRed[2].GetComponent<Image>().color = Color.green;
+
+        for (int i = 0; i < array5.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                array5[i] = i + 22;
+            }
+        }
+
+        for (int i = 0; i < array6.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                array6[i] = i + 2;
+            }
+        }
+
+        for (int i = 0; i < array7.Length; i++)
+        {
+            if (i % 2 == 1)
+            {
+                array7[i] = i;
+            }
+        }
+
+        for (int i = 0; i < array8.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                array8[i] = "a";
+            }
+            else if (i % 2 == 1)
+            {
+                array8[i] = "b";
+            }
+        }
+
+        for (int i = 0; i < array9.Length; i++)
+        {
+            array9[i] = Random.Range(0, 1000);
+        }
+        System.Array.Sort(array9);
+        for (int i = 0; i < array9.Length; i++)
+        {
+            Debug.Log(array9[i]);
+        }
+
+        int[] array10 = array5.Concat(array6).ToArray();
+
+        for (int i = 0; i < array11.Length; i++)
+        {
+            array11[i] = Random.Range(0, 1000);
+        }
+        System.Array.Sort(array11);
+        Debug.Log(array11[0] + " und " + array11[array11.Length - 1]);
+
+        for (int i = 0; i < array12.Length; i++)
+        {
+            array12[i] = Random.Range(0, 1000);
+        }
+        int randomNum = Random.Range(0, 1000);
+        Debug.Log(System.Array.IndexOf(array12, randomNum));
+    }
+
+
+
+
+    public void LangenString()
+    {
+
+        for (int i = 0; i < array3.Length; i++)
+        {
+            Debug.Log("array3[" + i + "] = " + array3[i]);
+        }
+        for (int i = 0; i < array3.Length; i++)
+        {
+            langenString = langenString  + " " + array3[i];
+        }
+        Debug.Log(langenString);
+
+
+
+    }
+}
